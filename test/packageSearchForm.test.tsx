7f9f298116fc441ad6231d06c047d607635cff63
Charles Foote <149430744+CharlesF127@users.noreply.github.com>
2025-04-24 02:07:21 -0400

    Update packageSearchForm.test.tsx

--- test/packageSearchForm.test.tsx
+++ test/packageSearchForm.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import PackageSearchForm from './src/components/PackageSearchForm';
+import PackageSearchForm from '../src/components/PackageSearchForm';
 import { toast } from 'sonner';
 import React from 'react';
 
