830ef32ce97bc7d0f16814b3f1d8aa8ffb3671c9
Charles Foote <149430744+CharlesF127@users.noreply.github.com>
2025-04-24 02:06:52 -0400

    Update packageResults.test.tsx

--- test/packageResults.test.tsx
+++ test/packageResults.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import PackageResults from './src/pages/PackageResults';
+import PackageResults from '../src/pages/PackageResults';
 
 describe('PackageResults', () => {
   it('renders loading state initially', () => {
